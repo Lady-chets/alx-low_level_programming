8b801a9b9525cd9b485ff3a5a579be8081ac4d76
Meekness <meeknesschets@gmail.com>
2022-07-02 23:46:46 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - puts
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 
-		puts("Programming is like building a multilingual puzzle\n");
-		return (0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
