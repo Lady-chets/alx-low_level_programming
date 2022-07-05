f385118efb05b20635f24bd4ac66eef6c61bd09d
Meekness <meeknesschets@gmail.com>
2022-07-04 17:19:50 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
 * main - puts
-*
 * Return: Always 0 (success)
 */
 int main(void)
 {
 
-puts(\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
